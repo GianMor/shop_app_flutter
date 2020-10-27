63a0370fc5c5eb0b9f7f3cc21b56a05a2d983086
Gianluca Moratto <gianluca.moratto@beantech.it>
2020-10-27 12:24:14 +0100

    ad model product

---
+++ lib/models/product.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/foundation.dart';
+
+class Product {
+  @required
+  final int id;
+  @required
+  final String title;
+  @required
+  final String description;
+  @required
+  final double price;
+  @required
+  final String imageUrl;
+  bool isFavorite;
+
+  Product(
+      {this.id,
+      this.title,
+      this.description,
+      this.price,
+      this.imageUrl,
+      this.isFavorite = false});
+}
