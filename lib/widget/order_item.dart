0ea3d2b288bf739e159c65d29968a6e9aba68e2b
Gianluca Moratto <gianluca.moratto@beantech.it>
2020-10-29 14:28:48 +0100

    add expand in order scren for orede

--- lib/widget/order_item.dart
+++ lib/widget/order_item.dart
@@ -1,25 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import '../providers/orders.dart' as ord;
+import 'dart:math';
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final ord.OrderItem order;
 
   const OrderItem(this.order);
 
+  @override
+  _OrderItemState createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  var _expanded = false;
   @override
   Widget build(BuildContext context) {
     return Card(
       margin: EdgeInsets.all(10),
       child: Column(children: [
         ListTile(
-          title: Text('\$${order.amount}'),
-          subtitle: Text(DateFormat.yMMMd().format(order.dateTime)),
+          title: Text('\$${widget.order.amount}'),
+          subtitle: Text(DateFormat.yMMMd().format(widget.order.dateTime)),
           trailing: IconButton(
-            icon: Icon(Icons.expand_more),
-            onPressed: () {},
+            icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+            onPressed: () {setState(() {
+              _expanded = !_expanded;
+            });}  
           ),
         ),
+        if (_expanded)
+          Container(
+            height: min(widget.order.products.length * 20.0 + 10, 180),
+            child: ListView(
+              children: widget.order.products
+                  .map((prod) => Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            prod.title,
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            '${prod.quantity}x \$${prod.price}',
+                            style: TextStyle(fontSize: 18, color: Colors.green),
+                          )
+                        ],
+                      ))
+                  .toList(),
+            ),
+          )
       ]),
     );
   }
