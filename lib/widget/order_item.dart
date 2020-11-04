5268e21b3c6d7d9c9b6776d07e5b8e76c1e081c9
Gianluca Moratto <gianluca.moratto@beantech.it>
2020-11-04 11:54:43 +0100

    add animation to orders

--- lib/widget/order_item.dart
+++ lib/widget/order_item.dart
@@ -16,23 +16,32 @@ class _OrderItemState extends State<OrderItem> {
   var _expanded = false;
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(10),
-      child: Column(children: [
-        ListTile(
-          title: Text('\$${widget.order.amount.toStringAsFixed(2)}'),
-          subtitle: Text(DateFormat.yMMMd().format(widget.order.dateTime)),
-          trailing: IconButton(
-              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
-              onPressed: () {
-                setState(() {
-                  _expanded = !_expanded;
-                });
-              }),
-        ),
-        if (_expanded)
-          Container(
-            height: min(widget.order.products.length * 20.0 + 10, 180),
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      height:
+          _expanded ? min(widget.order.products.length * 20.0 + 110, 200) : 95,
+      child: Card(
+        margin: EdgeInsets.all(10),
+        child: Column(children: [
+          ListTile(
+            title: Text('\$${widget.order.amount.toStringAsFixed(2)}'),
+            subtitle: Text(DateFormat.yMMMd().format(widget.order.dateTime)),
+            trailing: IconButton(
+                icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+                onPressed: () {
+                  setState(() {
+                    _expanded = !_expanded;
+                  });
+                }),
+          ),
+          // if (_expanded)
+          //   Container(
+          AnimatedContainer(
+            duration: Duration(milliseconds: 300),
+            // height: min(widget.order.products.length * 20.0 + 10, 180),
+            height: _expanded
+                ? min(widget.order.products.length * 20.0 + 10, 180)
+                : 0,
             child: ListView(
               children: widget.order.products
                   .map((prod) => Row(
@@ -52,7 +61,8 @@ class _OrderItemState extends State<OrderItem> {
                   .toList(),
             ),
           )
-      ]),
+        ]),
+      ),
     );
   }
 }
